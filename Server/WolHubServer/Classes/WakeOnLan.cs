ee0fbee636c539f0909fb9a9ed7f84fc4ff44c06
Alvaro Gonzalez Delgado <alvgonzadel@gmail.com>
2023-02-07 01:07:31 +0100

    Started API server

---
+++ Server/WolHubServer/Classes/WakeOnLan.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Globalization;
+
+namespace WolHubServer.Classes
+{
+    public class WakeOnLan
+    {
+        public string MacAddress { get; set; }
+
+        public WakeOnLan(string macAddress) { 
+            MacAddress = macAddress;
+        }
+
+
+        private void WakeUp()
+        {
+            UdpClient udpClient = new UdpClient();
+
+            //Habilitamos transmision UDP al cliente
+            udpClient.EnableBroadcast = true;
+
+            byte[] dgram = new byte[1024];
+
+            //6 magic bytes
+            for(int i=0; i<6; i++)
+            {
+                dgram[i] = 255;
+            }
+
+            //Convertir la dirección MAC a bytes
+            byte[] address_bytes = new byte[6];
+            for(int i=0; i<6; i++)
+            {
+                address_bytes[i] = byte.Parse(MacAddress.Substring(3 * i, 2), NumberStyles.HexNumber);
+            }
+
+            //Repetir la dirección MAC 16 veces en el datagram
+            var macaddress_block = dgram.AsSpan(6, 16 * 6);
+            for(int i=0; i < 16; i++)
+            {
+                address_bytes.CopyTo(macaddress_block.Slice(6 * i));
+            }
+
+            udpClient.Send(dgram, dgram.Length, new System.Net.IPEndPoint(IPAddress.Broadcast, 0));
+            udpClient.Close();
+        }
+    }
+}
---
+++ Server/WolHubServer/Controllers/WakeOnLanController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WolHubServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WakeOnLanController : ControllerBase
+    {
+
+        private readonly ILogger<WakeOnLanController> _logger;
+
+        public WakeOnLanController(ILogger<WakeOnLanController> logger) { 
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "PruebaApi")]
+        public string Get()
+        {
+            return "prueba";
+        }
+    }
+}
